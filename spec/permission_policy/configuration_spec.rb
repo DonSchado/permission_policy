a37d3a0390101cad11bddd175ca76514b0d50285
Marco Schaden <ms@donschado.de>
2014-10-10 23:55:05 +0200

    this is useless imo

--- spec/permission_policy/configuration_spec.rb
+++ spec/permission_policy/configuration_spec.rb
@@ -1,9 +1,6 @@
 module PermissionPolicy
   RSpec.describe Configuration do
     before { PermissionPolicy.configuration = nil }
-    subject { PermissionPolicy.config }
-
-    it { expect(subject).to be_kind_of(PermissionPolicy::Configuration) }
 
     it 'has a default precondition' do
       expect(subject.preconditions).to eq([:current_user])
